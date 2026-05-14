4e4ebbf7b6db51229ac596a383ec88fc125aa18d
Veedushi <136250260+Veedushi2101@users.noreply.github.com>
2026-05-14 19:33:51 +0530

    Reconnected the mongoDB server

---
+++ public/js/toggle.js
@@ -0,0 +1,5 @@
+let taxSwitch = document.getElementById("switchCheckDefault");
+taxSwitch.addEventListener("click", () => {
+  let taxInfo = document.getElementsByClassName("tax-info");
+  console.log(taxInfo);
+});
